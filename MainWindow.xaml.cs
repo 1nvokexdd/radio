e861ac0f2d71cffcda9a31913d14505e18572695
1nvokexdd <miskripa@gmail.com>
2026-04-16 17:39:40 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+using NAudio.Wave;
+
+
+namespace RadioPlayer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private IWavePlayer outputDevice;
+        private MediaFoundationReader reader;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+     }
+}
